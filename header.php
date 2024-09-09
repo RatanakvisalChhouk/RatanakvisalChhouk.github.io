221bf18666f4e213565d74663dcb4c557277df45
RatanakvisalChhouk <65413793+RatanakvisalChhouk@users.noreply.github.com>
2024-09-09 13:32:24 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<header class = "bg-success" id="top">
+        <nav>
+        <a href="https://www.linkedin.com/in/kirirom-food-production-k-f-p-co-ltd-959691221/"><img src="Images/linkedin.png" alt="linkedin" width="25px" height="25px"></a>
+        <a href="https://web.facebook.com/kiriromfoodproduction"><img src="Images/fb.png" alt="facebook" width="25px" height="25px"></a>
+        <a href="https://www.youtube.com/channel/UCiLPpp-iP4B1ezXT1E2XikA"><img src="Images/yt.png" alt="youtube" width="25px" height="25px"></a>
+            <ul class="navbar-nav">
+                <li>sale@kfp.com.kh</li>
+                <li>(+855) 23 977 988</li>
+            </ul>
+        </nav>
+</header>
+
